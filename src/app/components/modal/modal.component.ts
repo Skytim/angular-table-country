72e5239ba5f46b078f34995d2efa2cc0c81230bd
Skytim <sky22357168@gmail.com>
2021-06-22 15:42:29 +0800

    Add Modal and Add Folder

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { PeriodicElement } from '../table-basic-example';
+import { PeriodicElement } from '../../model/periodic-element';
 
 @Component({
   selector: 'app-modal',
